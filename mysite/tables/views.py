e32ae90c292b52309dbb190c76b3c0e93cc76d79
BetterCallSaulM <saulmachuca56@gmail.com>
2024-10-22 18:58:18 -0700

    Added remaining user endpoints

--- mysite/tables/views.py
+++ mysite/tables/views.py
@@ -80,6 +80,132 @@ class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
+    @action(detail=False, methods=['GET'], url_path='movie')
+    def get_movie(self, request):
+        movie_name = request.query_params.get('name')
+
+        if not movie_name:
+            return Response({'error' : 'Movie name required.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            movie_data = Movie.objects.get(title=movie_name)
+            serializer = MovieSerializer(movie_data)
+            return Response({'message' : 'Movie retrieved successfully', 'movie' : serializer.data}, status=status.HTTP_202_ACCEPTED)
+        except Watchlist.DoesNotExist:
+            return Response({'error': 'Movie not found'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({'error': f'{e}'}, status=status.HTTP_400_BAD_REQUEST)
+        
+    @action(detail=False, methods=['GET'], url_path='movies')
+    def get_movies(self, request):
+        movie_name = request.query_params.get('name')
+
+        if not movie_name:
+            return Response({'error' : 'Movie name required.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            movies = Movie.objects.filter(title__icontains=movie_name).values()
+            return Response({'message' : 'Movies retrieved successfully', 'movie(s)' : movies}, status=status.HTTP_202_ACCEPTED)
+        except Movie.DoesNotExist:
+            return Response({'error': 'Movie(s) not found'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({'error': f'{e}'}, status=status.HTTP_400_BAD_REQUEST)
+        
+    @action(detail=False, methods=['POST'], url_path='add')
+    def add_movie(self, request):
+        movie_name = request.query_params.get('name')
+
+        if not movie_name:
+            return Response({'error' : 'Movie name required.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        year = request.query_params.get('year', Movie._meta.get_field('year').get_default())
+        director = request.query_params.get('director', Movie._meta.get_field('director').get_default())
+        genre = request.query_params.get('genre', Movie._meta.get_field('genre').get_default())
+        poster = request.query_params.get('poster', Movie._meta.get_field('poster').get_default())
+
+        movie_data = {
+            'title' : movie_name,
+            'year' : year,
+            'director' : director,
+            'genre' : genre,
+            'poster' : poster
+        }
+        serializer = MovieSerializer(data=movie_data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    @action(detail=False, methods=['DELETE'], url_path='delete')
+    def delete_movie(self, request):
+        movie_name = request.query_params.get('name')  
+
+        if not movie_name:
+            return Response({'error': 'Movie title is required.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            movie_data = Movie.objects.get(title=movie_name)
+            movie_data.delete()
+            return Response({'message' : 'Movie deleted successfully'}, status=status.HTTP_202_ACCEPTED)
+        except User.DoesNotExist:
+            return Response({'error': 'Movie not found'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({'error': f'{e}'}, status=status.HTTP_400_BAD_REQUEST)
+        
+    @action(detail=False, methods=['PATCH'], url_path='edit')
+    def edit_movie(self, request):
+        movie_name = request.query_params.get('name')
+
+        if not movie_name:
+            return Response({'error': 'Movie name is required.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            movie = Movie.objects.get(title=movie_name)
+        except Movie.DoesNotExist:
+            return Response({'error': 'Movie not found.'}, status=status.HTTP_404_NOT_FOUND)
+        
+        title = request.query_params.get('title')
+        if title is not None:
+            movie.title = title
+        
+        year = request.query_params.get('year')
+        if year is not None:
+            movie.year = year
+        
+        director = request.query_params.get('director')
+        if director is not None:
+            movie.director = director
+
+        genre = request.query_params.get('genre')
+        if genre is not None:
+            movie.genre = genre
+
+        poster = request.query_params.get('poster')
+        if poster is not None:
+            movie.poster = poster
+
+        movie.save()
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
 class WatchlistViewSet(viewsets.ModelViewSet):
     queryset = Watchlist.objects.all()
     serializer_class = WatchlistSerializer
+
+    @action(detail=False, methods=['GET'], url_path='list')
+    def get_list(self, request):
+        list_name = request.query_params.get('list_name')
+        username = request.query_params.get('user')
+
+        if not list_name or not username:
+            return Response({'error' : 'List name and username required.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            list_data = Watchlist.objects.filter(watchlist_name=list_name, username=username).values()
+            return Response({'message' : 'List retrieved successfully', 'watchlist' : list_data}, status=status.HTTP_202_ACCEPTED)
+        except Watchlist.DoesNotExist:
+            return Response({'error': 'Watchlist not found'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({'error': f'{e}'}, status=status.HTTP_400_BAD_REQUEST)
